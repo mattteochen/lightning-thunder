abd15b7cac5532860434f3dc563c15de06223598
matteochen <kaiximatteo.chen@mail.polimi.it>
2024-07-19 13:05:39 +0200

    Updated test

--- examples/dev/litGPT.py
+++ examples/dev/litGPT.py
@@ -4,13 +4,12 @@ import thunder
 import torch
 
 cfg = Config.from_name('Llama-2-7b-hf')
-cfg.n_layer = 16 # fewer layers
+cfg.n_layer = 8 # fewer layers
 torch.set_default_dtype(torch.bfloat16)
 with torch.device('cuda'):
     m = GPT(cfg)
-    thunder_model = thunder.jit(m)
-
-    inp = torch.randint(1, m.config.vocab_size, (1, 512))
-
-    actual = thunder_model(inp)
-    expected = m(inp)
+    x = torch.randint(1, m.config.vocab_size, (1, 512))
+    jmodel_def = thunder.jit(m)
+    jmodel_auto = thunder.jit(m, autotune_type='runtime')
+    y = jmodel_def(x)
+    yy = jmodel_auto(x)
