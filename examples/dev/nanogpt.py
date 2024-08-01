ce3a74dcabacd5ab8657710c756a798ad416c0a3
matteochen <kaiximatteoc@nvidia.com>
2024-08-01 12:24:15 +0300

    Updated nano

--- examples/dev/nanogpt.py
+++ examples/dev/nanogpt.py
@@ -14,7 +14,7 @@ def bench():
     batch_size = 12
     block_size = 1024
     bias = False
-    real_data = True
+    real_data = False
     seed = 1337
     device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
     dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32' or 'bfloat16' or 'float16'
