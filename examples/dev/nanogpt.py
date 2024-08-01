06c8d8f1d2236c5ed88db6ab220ec8d3eacc4834
matteochen <kaiximatteoc@nvidia.com>
2024-08-01 12:23:19 +0300

    Updated nano

--- examples/dev/nanogpt.py
+++ examples/dev/nanogpt.py
@@ -20,7 +20,7 @@ def bench():
     dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32' or 'bfloat16' or 'float16'
     compile = False # use PyTorch 2.0 to compile the model to be faster
     profile = False # use pytorch profiler, or just simple benchmarking?
-    exec(open('configurator.py').read()) # overrides from command line or config file
+    # exec(open('configurator.py').read()) # overrides from command line or config file
     # -----------------------------------------------------------------------------
 
     torch.manual_seed(seed)
